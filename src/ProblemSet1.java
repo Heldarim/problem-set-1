5e097ccf8f5667230dbf26edf30bcac416846c67
Jason Maher <jmahercollege@gmail.com>
2018-09-24 08:33:46 -0400

    first commit

--- src/ProblemSet1.java
+++ src/ProblemSet1.java
@@ -15,13 +15,31 @@ public class ProblemSet1 {
 	
 	public static void main(String[] args) {
 		System.out.println("/// EXERCISE 1 /////////////////////////////////////////////////\n");
+		System.out.println("   ##    ######  ######## ########");
+		System.out.println("  #  #   #     # #        #");
+		System.out.println(	" #    #  #     # #        #");
+		System.out.println("######## ######  #        ########"); 
+		System.out.println("#      # #       #               #");
+		System.out.println("#      # #       #               #");
+		System.out.println("#      # #       ######## ########");
 		
 		System.out.println("/// EXERCISE 2 /////////////////////////////////////////////////\n");
-		
+
+		System.out.println("  /\\     /\\     /\\");
+		System.out.println(" /  \\   /  \\   /  \\");
+		System.out.println("/    \\ /    \\ /    \\");
+		System.out.println("\\    / \\    / \\    /");
+		System.out.println(" \\  /   \\  /   \\  /");
+		System.out.println("  \\/     \\/     \\/");
 		System.out.println("/// EXERCISE 3 /////////////////////////////////////////////////\n");
-		
-		System.out.println("/// EXERCISE 4 /////////////////////////////////////////////////\n");
-		
-		System.out.println("/// EXERCISE 5 /////////////////////////////////////////////////\n");
+		System.out.print("   \\\\\\\\\\ \n");
+		System.out.print("  +\"\"\"\"\"+ \n");	
+		System.out.print("(|  0 0  |) \n");
+		System.out.print(" |   ^   | \n");
+		System.out.print(" |  '-'  | \n");
+		System.out.print("  +-----+ \n");
+		System.out.println("/// EXERCISE 4 /////////////////////////////////////////////////\n H E L L O \n E       A \n L       P \n L       C \n O A P C S \n");
+		System.out.println("/// EXERCISE 5 /////////////////////////////////////////////////\n \"The answer's quite simple,\" the professor said. \"Just think outside the box!\"");
 	}
 }
+
